3f04fd11d5f31f830922d63c60f36bea5f49399f
Ilias Kladakis <iliask@uw.edu>
2025-04-20 16:09:55 -0700

    testing css styling

--- components/pages/landing/landing.js
+++ components/pages/landing/landing.js
@@ -1,10 +1,10 @@
-
+import "./landing.css";
 
 
 function landing() {
     return (
         <div>
-            <h1>Vinny was here</h1>
+            <h1 className="default-header">Vinny was here</h1>
             <p>for the lamb</p>
             <p>and he pushed (testing out branch)</p>
         </div>
