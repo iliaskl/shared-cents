40b838a4594ac9aed3848c51bf43928ea0ef3e39
Ilias Kladakis <iliask@uw.edu>
2025-04-20 15:26:15 -0700

    starting code

--- app/analytics/page.jsx
+++ app/analytics/page.jsx
@@ -1,3 +1,5 @@
+import LandingPage from "../../components/pages/landing/landing.js"
+
 export default function hell0() {
     return (<>hello <h1> hello 2</h1> </>)
 }
---
+++ components/pages/landing/landing.js
@@ -0,0 +1,13 @@
+
+
+
+function landing() {
+    return (
+        <div>
+            <h1>Vinny was here</h1>
+            <p>for the lamb</p>
+        </div>
+    );
+};
+
+export default landing;
